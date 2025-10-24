e8566e60d22dd49cffed0c9e6356b57306a786ea
MoyukhShabon <moyukh.work@gmail.com>
2025-10-24 17:59:13 +0800

    updated seqc2 filenames

--- analysis/seqc2/sync.sh
+++ analysis/seqc2/sync.sh
@@ -1,6 +1,6 @@
 #!/usr/env/bin bash
 
-rsync -chaPv ../../../eval/mutect2-matched-normal_pass-orientation-filtered.vs.filtered-ff/FFG/roc-prc-auc/precrec/combined_auc_table.tsv seqc2.ffg.f-ffpe_f-ff.tsv
-rsync -chaPv ../../../eval/mutect2-matched-normal_pass-orientation-filtered.vs.filtered-ff/FFX/roc-prc-auc/precrec/combined_auc_table.tsv seqc2.ffx.f-ffpe_f-ff.tsv
-rsync -chaPv ../../../eval/mutect2-matched-normal_pass-orientation-filtered.vs.unfiltered-ff/FFG/roc-prc-auc/precrec/combined_auc_table.tsv seqc2.ffg.f-ffpe_uf-ff.tsv
-rsync -chaPv ../../../eval/mutect2-matched-normal_pass-orientation-filtered.vs.unfiltered-ff/FFX/roc-prc-auc/precrec/combined_auc_table.tsv seqc2.ffx.f-ffpe_uf-ff.tsv
+rsync -chaPv ../../../eval/mutect2-matched-normal_pass-orientation-filtered.vs.filtered-ff/FFG/roc-prc-auc/precrec/combined_auc_table.tsv seqc2.ffg.matched.f-ffpe_f-ff.tsv
+rsync -chaPv ../../../eval/mutect2-matched-normal_pass-orientation-filtered.vs.filtered-ff/FFX/roc-prc-auc/precrec/combined_auc_table.tsv seqc2.ffx.matched.f-ffpe_f-ff.tsv
+rsync -chaPv ../../../eval/mutect2-matched-normal_pass-orientation-filtered.vs.unfiltered-ff/FFG/roc-prc-auc/precrec/combined_auc_table.tsv seqc2.ffg.matched.f-ffpe_uf-ff.tsv
+rsync -chaPv ../../../eval/mutect2-matched-normal_pass-orientation-filtered.vs.unfiltered-ff/FFX/roc-prc-auc/precrec/combined_auc_table.tsv seqc2.ffx.matched.f-ffpe_uf-ff.tsv
