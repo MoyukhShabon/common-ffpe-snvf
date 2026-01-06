2b2736cde6458e954edde3c60f8f6d6480a87c7b
MoyukhShabon <moyukh.work@gmail.com>
2026-01-06 18:37:22 +0800

    added docstrings to common.py

--- python/common.py
+++ python/common.py
@@ -4,7 +4,21 @@ from scipy.stats import false_discovery_control
 from sklearn.metrics import confusion_matrix
 
 
-def read_variants(path:str, columns: list = ["#CHROM", "POS", "REF", "ALT", "FILTER"]) -> pl.DataFrame:
+def read_variants(path:str, columns: list = ["#CHROM", "POS", "REF", "ALT"]) -> pl.DataFrame:
+	"""
+	Reads Variants from a VCF file into a Polars DataFrame.
+	By default, only reads essential columns.
+	Splits multi-allelic variants into separate rows.
+	Can be extended to read additional columns as needed or
+	read custom variant files with similar structure.
+	
+	:param path: Path to VCF file
+	:type path: str
+	:param columns: List of column names to read from the VCF file. Defaults to ["#CHROM", "POS", "REF", "ALT"].
+	:type columns: list
+	:return: DataFrame containing the variants
+	:rtype: DataFrame
+	"""
 	variants = (
 		pl.read_csv(path, separator="\t", comment_prefix="##", infer_schema_length=1000, columns=columns)
 		.rename(lambda x: x.lstrip("#").lower())
@@ -15,6 +29,18 @@ def read_variants(path:str, columns: list = ["#CHROM", "POS", "REF", "ALT", "FIL
 
 
 def natural_sort_variants(df: pl.DataFrame, chr_col: str = "chrom", pos_col: str = "pos") -> pl.DataFrame:
+	"""
+	Docstring for natural_sort_variants
+	
+	:param df: Polars DataFrame containing variants
+	:type df: pl.DataFrame
+	:param chr_col: Name of the chromosome column
+	:type chr_col: str
+	:param pos_col: Name of the position column
+	:type pos_col: str
+	:return: DataFrame containing the variants sorted naturally by chromosome and position
+	:rtype: DataFrame
+	"""
 	return (
 		df.with_columns(
 			# Create a temporary column 'chr_rank' for sorting
@@ -33,22 +59,39 @@ def natural_sort_variants(df: pl.DataFrame, chr_col: str = "chrom", pos_col: str
 	)
 
 
-def adaptive_fdr_cut(df: pl.DataFrame, fp_cut: float) -> pl.DataFrame:
+def adaptive_fdr_cut(df: pl.DataFrame, fp_cut: float, score_col: str = "q") -> pl.DataFrame:
 	"""
 	Takes a DataFrame containing a 'q' column, calculates the adaptive cutoff metric,
 	and adds a boolean 'pred' column using Polars' built-in rank method. Applies a 
 	cutoff where the expected number of false positives in the selected set is less 
 	than a specific number defined by fp_cut
+	
+	:param df: DataFrame containing model (MOBSNVF) scores
+	:type df: pl.DataFrame
+	:param fp_cut: False positive cutoff threshold
+	:type fp_cut: float
+	:param score_col: Name of the score column
+	:type score_col: str
+	:return: DataFrame with a boolean 'pred' column indicating predictions
+	:rtype: DataFrame
 	"""
 	return df.with_columns(
-		((pl.col("q").rank(method="ordinal") * pl.col("q")) < fp_cut).alias("pred")
+		((pl.col(score_col).rank(method="ordinal") * pl.col(score_col)) < fp_cut).alias("pred")
 	)
-
 
 
 def fdr_cut_pred(df: pl.DataFrame, score_col: str, fp_cut: float = 0.5) -> pl.DataFrame:
 	"""
 	Labels model predictions based on adaptive FDR cut using SciPy for BH correction.
+	
+	:param df: Polars DataFrame containing model (MOBSNVF) scores
+	:type df: pl.DataFrame
+	:param score_col: Name of the score column
+	:type score_col: str
+	:param fp_cut: False positive cutoff threshold
+	:type fp_cut: float
+	:return: DataFrame with a boolean 'pred' column indicating predictions
+	:rtype: DataFrame
 	"""
 	
 	# Split Complete Cases (C>T) and Nulls
@@ -97,19 +140,29 @@ def fdr_cut_pred(df: pl.DataFrame, score_col: str, fp_cut: float = 0.5) -> pl.Da
 
 
 def get_eval_metrics(truth: pl.Series, pred: pl.Series) -> dict:
+	"""
+	Calculates evaluation metrics based on truth and predicted labels.
+	
+	:param truth: Series containing true labels
+	:type truth: pl.Series
+	:param pred: Series containing predicted labels
+	:type pred: pl.Series
+	:return: Dictionary containing evaluation metrics
+	:rtype: dict
+	"""
 
-    cm = confusion_matrix(truth, pred)
-    TN, FP, FN, TP = cm.ravel()
+	cm = confusion_matrix(truth, pred)
+	TN, FP, FN, TP = cm.ravel()
 
-    eval_metrics = {
-        "precision": TP / (TP + FP),
-        "recall": TP / (TP + FN),
-        "specificity": TN / (TN + FP),
-        "TP": TP,
-        "TN": TN,
-        "FP": FP,
-        "FN": FN,
-        "confusion_matrix": cm
-    }
+	eval_metrics = {
+		"precision": TP / (TP + FP),
+		"recall": TP / (TP + FN),
+		"specificity": TN / (TN + FP),
+		"TP": TP,
+		"TN": TN,
+		"FP": FP,
+		"FN": FN,
+		"confusion_matrix": cm
+	}
 
-    return eval_metrics
+	return eval_metrics
