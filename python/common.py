3136d724286e92325252198f5d3a84dd903251ce
MoyukhShabon <moyukh.work@gmail.com>
2026-01-21 01:23:26 +0800

    updated common python module

--- python/common.py
+++ python/common.py
@@ -27,6 +27,18 @@ def read_variants(path:str, columns: list = ["#CHROM", "POS", "REF", "ALT"]) -> 
 	)
 	return variants
 
+def snv_filter(variants: pl.DataFrame) -> pl.DataFrame:
+    return variants.filter(
+        (pl.col("ref").str.len_chars() == 1) &
+        (pl.col("alt").str.len_chars() == 1)
+	)
+
+def ct_filter(variants: pl.DataFrame) -> pl.DataFrame:
+    return variants.filter(
+        ((pl.col("ref").str.to_uppercase() == "C") & (pl.col("alt").str.to_uppercase() == "T")) |
+        ((pl.col("ref").str.to_uppercase() == "G") & (pl.col("alt").str.to_uppercase() == "A"))
+	)
+
 
 def natural_sort_variants(df: pl.DataFrame, chr_col: str = "chrom", pos_col: str = "pos") -> pl.DataFrame:
 	"""
