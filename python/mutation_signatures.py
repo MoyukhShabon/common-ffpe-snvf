cc42eb86cd8aae4af9a03a11c47de1b568555b52
MoyukhShabon <moyukh.work@gmail.com>
2026-01-28 20:30:37 +0800

    updated mutation signatures module

--- python/mutation_signatures.py
+++ python/mutation_signatures.py
@@ -26,8 +26,7 @@ import pysam
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import use
-use('Agg')
+from common import snv_filter
 
 def import_formatted_vcf(vcf_path, sample_name, snv_only = True, normal_chr=True) -> pl.DataFrame:
 	
@@ -139,16 +138,19 @@ def collapse_context_96c(x):
 	return x[:mid] + new_mid + x[mid+1:]
 
 
-def variants_mut_profile(variant_path, sample_name, genome, c96=True):
+def variants_mut_profile(snv, genome, sample_name = None, c96=True):
 
-	snv = import_formatted_vcf(variant_path, sample_name)
+	snv = snv.pipe(snv_filter)
+
+	if sample_name:
+		snv = snv.with_columns(pl.lit(sample_name).alias("sample_name"))
+
 	## obtain mutation type
 	snv = snv.with_columns([
 		pl.concat_str([pl.col("ref"), pl.lit(">"), pl.col("alt")]).alias("mutation_type"),
 	])
 	
 	## Obtain mutation trinucleotide context
-	### map_elements is vectorized and is faster than a for loop
 	snv = snv.with_columns([
 		pl.struct(["chrom", "pos", "ref"]).map_elements(lambda row: get_context(row, genome), return_dtype=pl.String).alias("trinucleotide"),
 	])
@@ -183,22 +185,31 @@ def create_96c_mutation_counts(snv_96c):
 
 	channels_df = pl.DataFrame(channels, schema=["mutation_type", "trinucleotide"], orient="row")
 	
-	mut_matrix_96c = (
-		snv_96c
-		.group_by(["mutation_type", "trinucleotide", "sample_id"])
-		.len()
-		.pivot(
-			values="len",
-			index=["mutation_type", "trinucleotide"],
-			on="sample_id"
+	if "sample_name" in snv_96c.columns:
+		mut_matrix_96c = (
+			snv_96c
+			.group_by(["mutation_type", "trinucleotide", "sample_name"])
+			.len()
+			.pivot(
+				values="len",
+				index=["mutation_type", "trinucleotide"],
+				on="sample_name"
+			)
+			.fill_null(0)
 		)
-		.fill_null(0)
-	)
+		# Sort sample columns
+		index_cols = ["mutation_type", "trinucleotide"]
+		sample_cols = sorted([col for col in mut_matrix_96c.columns if col not in index_cols])
+		mut_matrix_96c = mut_matrix_96c.select(index_cols + sample_cols).sort(index_cols)
 
-	# Sort sample columns
-	index_cols = ["mutation_type", "trinucleotide"]
-	sample_cols = sorted([col for col in mut_matrix_96c.columns if col not in index_cols])
-	mut_matrix_96c = mut_matrix_96c.select(index_cols + sample_cols).sort(index_cols)
+	else:
+		mut_matrix_96c = (
+			snv_96c
+			.group_by(["mutation_type", "trinucleotide"])
+			.len()
+			.rename({"len": "count"})
+			.fill_null(0)
+		)
 	
 	mut_matrix_96c = channels_df.join(mut_matrix_96c, on=["mutation_type", "trinucleotide"], how="left").fill_null(0).sort(["mutation_type", "trinucleotide"])
 	
@@ -206,112 +217,129 @@ def create_96c_mutation_counts(snv_96c):
 
 
 	
-def SBS96_plot(sig, label = "", name = "", file = False, norm = False,
-               width = 10, height = 2, bar_width = 1, 
-               xticks_label = False, grid = 0.2, s = 10):
-    """
-    This function plots 96-channel profile for a given signature or mutational catalogue.   
-    Required:
-        sig: 96-channel mutation counts/probabilities
-    Optional arguments(default values see above):
-        label: to identify the plot, e.g. sample ID
-        name: to add extra information inside of the plot, e.g. "Biological"
-        file: file name where to save the plot if given
-        norm: to normlize provided 96-channel vector or not
-        width: width of the plot
-        height: height of the plot
-        bar_width: bar_width of the plot
-        xticks_label: to show the xticks channel information or not
-        grid: grid of the plot
-        s: fontsize of the figure main text    
-    """
-    
-    channel = 96
-    col_set = ['deepskyblue','black','red','lightgrey','yellowgreen','pink']
-    col_list = []
-    for i in range (len(col_set)):
-        col_list += [col_set[i]] * 16
-    
-    sns.set(rc={"figure.figsize":(width, height)})
-    sns.set(style="whitegrid", color_codes=True, rc={"grid.linewidth": grid, 'grid.color': '.7', 'ytick.major.size': 2,
-                                                 'axes.edgecolor': '.3', 'axes.linewidth': 1.35,})
+def SBS96_plot(sig, label = "", name = "", file = None, norm = False,
+			   width = 10, height = 2, bar_width = 1, 
+			   xticks_label = False, grid = 0.2, s = 10, ylim = None):
+	"""
+	This function plots 96-channel profile for a given signature or mutational catalogue.   
+	Required:
+		sig: 96-channel mutation counts/probabilities
+	Optional arguments(default values see above):
+		label: to identify the plot, e.g. sample ID
+		name: to add extra information inside of the plot, e.g. "Biological"
+		file: file name where to save the plot if given
+		norm: to normlize provided 96-channel vector or not
+		width: width of the plot
+		height: height of the plot
+		bar_width: bar_width of the plot
+		xticks_label: to show the xticks channel information or not
+		grid: grid of the plot
+		s: fontsize of the figure main text    
+	"""
+	
+	channel = 96
+	col_set = ['deepskyblue','black','red','lightgrey','yellowgreen','pink']
+	col_list = []
+	for i in range (len(col_set)):
+		col_list += [col_set[i]] * 16
+	
+	# This ensures we never draw on top of an old plot
+	plt.figure(figsize=(width, height))
+	sns.set_theme(style="whitegrid", color_codes=True, rc={"grid.linewidth": grid, 'grid.color': '.7', 'ytick.major.size': 2,
+												 'axes.edgecolor': '.3', 'axes.linewidth': 1.35,})
 
-    channel6 = ['C>A','C>G','C>T','T>A','T>C','T>G']
-    channel96 = ['ACA', 'ACC', 'ACG', 'ACT', 'CCA', 'CCC', 'CCG', 'CCT', 'GCA',
-               'GCC', 'GCG', 'GCT', 'TCA', 'TCC', 'TCG', 'TCT', 'ACA', 'ACC',
-               'ACG', 'ACT', 'CCA', 'CCC', 'CCG', 'CCT', 'GCA', 'GCC', 'GCG',
-               'GCT', 'TCA', 'TCC', 'TCG', 'TCT', 'ACA', 'ACC', 'ACG', 'ACT',
-               'CCA', 'CCC', 'CCG', 'CCT', 'GCA', 'GCC', 'GCG', 'GCT', 'TCA',
-               'TCC', 'TCG', 'TCT', 'ATA', 'ATC', 'ATG', 'ATT', 'CTA', 'CTC',
-               'CTG', 'CTT', 'GTA', 'GTC', 'GTG', 'GTT', 'TTA', 'TTC', 'TTG',
-               'TTT', 'ATA', 'ATC', 'ATG', 'ATT', 'CTA', 'CTC', 'CTG', 'CTT',
-               'GTA', 'GTC', 'GTG', 'GTT', 'TTA', 'TTC', 'TTG', 'TTT', 'ATA',
-               'ATC', 'ATG', 'ATT', 'CTA', 'CTC', 'CTG', 'CTT', 'GTA', 'GTC',
-               'GTG', 'GTT', 'TTA', 'TTC', 'TTG', 'TTT']
-    
-    ## plot the normalized version:
-    if norm:
-        normed_sig = sig / np.sum(sig)
-        plt.bar(range(channel), normed_sig , width = bar_width, color = col_list)
-        plt.xticks(rotation = 90, size = 7, weight = 'bold')
-        plt.ylim (0, np.max(normed_sig) * 1.15)
-        plt.annotate (name,(90 - len(name), np.max(sig) * 0.95), size = s)
-        plt.ylabel("Frequency")
+	channel6 = ['C>A','C>G','C>T','T>A','T>C','T>G']
+	channel96 = ['ACA', 'ACC', 'ACG', 'ACT', 'CCA', 'CCC', 'CCG', 'CCT', 'GCA',
+			   'GCC', 'GCG', 'GCT', 'TCA', 'TCC', 'TCG', 'TCT', 'ACA', 'ACC',
+			   'ACG', 'ACT', 'CCA', 'CCC', 'CCG', 'CCT', 'GCA', 'GCC', 'GCG',
+			   'GCT', 'TCA', 'TCC', 'TCG', 'TCT', 'ACA', 'ACC', 'ACG', 'ACT',
+			   'CCA', 'CCC', 'CCG', 'CCT', 'GCA', 'GCC', 'GCG', 'GCT', 'TCA',
+			   'TCC', 'TCG', 'TCT', 'ATA', 'ATC', 'ATG', 'ATT', 'CTA', 'CTC',
+			   'CTG', 'CTT', 'GTA', 'GTC', 'GTG', 'GTT', 'TTA', 'TTC', 'TTG',
+			   'TTT', 'ATA', 'ATC', 'ATG', 'ATT', 'CTA', 'CTC', 'CTG', 'CTT',
+			   'GTA', 'GTC', 'GTG', 'GTT', 'TTA', 'TTC', 'TTG', 'TTT', 'ATA',
+			   'ATC', 'ATG', 'ATT', 'CTA', 'CTC', 'CTG', 'CTT', 'GTA', 'GTC',
+			   'GTG', 'GTT', 'TTA', 'TTC', 'TTG', 'TTT']
+	
+	## plot the normalized version:
+	if norm:
+		normed_sig = sig / np.sum(sig)
+		plt.bar(range(channel), normed_sig , width = bar_width, color = col_list)
+		plt.xticks(rotation = 90, size = 7, weight = 'bold')
+		if ylim is not None:
+			plt.ylim(0, ylim)
+		else:
+			plt.ylim (0, np.max(normed_sig) * 1.15)
+		plt.annotate (name,(90 - len(name), np.max(sig) * 0.95), size = s)
+		plt.ylabel("Frequency")
 
-    ## plot the original version:
-    else:
-        plt.bar(range(channel), sig , width = bar_width, color =col_list)
-        plt.xticks(rotation = 90, size = 7, weight = 'bold')
-        plt.ylim(0,np.max(sig) * 1.15)
-        
-        if  np.round(np.sum (sig)) != 1:
-            plt.annotate(f"Total Count : {np.sum(sig):,}\nC>T Count : {np.sum(sig[32:48]):,}", (0, np.max(sig)-(np.max(sig)*0.1)), size=s*1.5)
-        plt.ylabel("Number of\nSBSs", size = s*2)
-        plt.annotate (name,(90 - len(name), np.max(sig) * 0.95), size = s)
-    
-    if xticks_label:
-        plt.xticks(range(channel), channel96, rotation = 90, ha = "center", va= "center",  size = 7)
-    else:
-        plt.xticks([], [])
-        
-    plt.yticks( va= "center",  size = s)
-    
-    ## plot the bar annotation:
-    text_col = ["w","w","w","black","black","black"]
-    for i in range(6):       
-        left, width = 0 + 1/6 * i + 0.001, 1/6 - 0.002
-        bottom, height = 1.003, 0.15
-        right = left + width
-        top = bottom + height
-        ax = plt.gca()
-        p = plt.Rectangle((left, bottom), width, height, fill=True, color = col_set[i])
-        p.set_transform(ax.transAxes)
-        p.set_clip_on(False)
-        ax.add_patch(p)
-        ax.text(0.5 * (left + right), 0.5 * (bottom + top), channel6[i], 
-                color = text_col[i], weight='bold',size = s*1.8,
-                horizontalalignment='center',verticalalignment='center', 
-                transform=ax.transAxes)
-    
-    ## plot the name annotation
-    if label != "":
-        left, width = 1.003, 0.05
-        bottom, height = 0, 1
-        right = left + width
-        top = bottom + height
-        ax = plt.gca()
-        p = plt.Rectangle((left, bottom), width, height, fill = True, color = "silver",alpha = 0.3)
-        p.set_transform(ax.transAxes)
-        p.set_clip_on(False)
-        ax.add_patch(p)
-        ax.text(0.505 * (left + right), 0.5 * (bottom + top), label, color = "black",size = s,
-                horizontalalignment='center',verticalalignment='center',
-                transform=ax.transAxes , rotation = 90)
+	## plot the original version:
+	else:
+		plt.bar(range(channel), sig , width = bar_width, color =col_list)
+		plt.xticks(rotation = 90, size = 7, weight = 'bold')
+		if ylim is not None:
+			plt.ylim(0, ylim * 1.15)
+		else:
+			plt.ylim(0,np.max(sig) * 1.15)
+		
+		if  np.round(np.sum (sig)) != 1:
+			plt.annotate(
+				f"Total Count : {np.sum(sig):,}\nC>T Count : {np.sum(sig[32:48]):,}", 
+				xy=(0.02, 0.95), 
+				xycoords='axes fraction', 
+				size=s*1.5,
+				verticalalignment='top'
+			)
+		plt.ylabel("Number of\nSBSs", size = s*2)
+		plt.annotate (name,(90 - len(name), np.max(sig) * 0.95), size = s)
+	
+	if xticks_label:
+		plt.xticks(range(channel), channel96, rotation = 90, ha = "center", va= "center",  size = 7)
+	else:
+		plt.xticks([], [])
+		
+	plt.yticks( va= "center",  size = s)
+	
+	## plot the bar annotation:
+	text_col = ["w","w","w","black","black","black"]
+	for i in range(6):       
+		left, width = 0 + 1/6 * i + 0.001, 1/6 - 0.002
+		bottom, height = 1.003, 0.15
+		right = left + width
+		top = bottom + height
+		ax = plt.gca()
+		p = plt.Rectangle((left, bottom), width, height, fill=True, color = col_set[i])
+		p.set_transform(ax.transAxes)
+		p.set_clip_on(False)
+		ax.add_patch(p)
+		ax.text(0.5 * (left + right), 0.5 * (bottom + top), channel6[i], 
+				color = text_col[i], weight='bold',size = s*1.8,
+				horizontalalignment='center',verticalalignment='center', 
+				transform=ax.transAxes)
+	
+	## plot the name annotation
+	if label != "":
+		left, width = 1.003, 0.05
+		bottom, height = 0, 1
+		right = left + width
+		top = bottom + height
+		ax = plt.gca()
+		p = plt.Rectangle((left, bottom), width, height, fill = True, color = "silver",alpha = 0.3)
+		p.set_transform(ax.transAxes)
+		p.set_clip_on(False)
+		ax.add_patch(p)
+		ax.text(0.505 * (left + right), 0.5 * (bottom + top), label, color = "black", size = s*2,
+				horizontalalignment='center',verticalalignment='center',
+				transform=ax.transAxes , rotation = 90)
 
-    ax.margins(x=0.002, y=0.002)
-    plt.tight_layout()
-    if file:
-        plt.savefig(file, bbox_inches = "tight", dpi = 300); f,ax = plt.subplots()
+	ax.margins(x=0.002, y=0.002)
+	plt.tight_layout()
+	if file:
+		plt.savefig(file, bbox_inches = "tight", dpi = 300)
+		plt.close()
+	else:
+		plt.show()
+		plt.close()
 
 	
 ## A polars mask for filtering C:G>T:A mutations. Invertable using ~
